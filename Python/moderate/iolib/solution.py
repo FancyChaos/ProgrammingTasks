03658082ccbf5d1d96efb763d30f5fe468b4f1f9
Lars Bergmann <lb@perfact.de>
2019-08-02 19:21:23 +0200

    Improve iolib task

--- Python/moderate/iolib/solution.py
+++ Python/moderate/iolib/solution.py
@@ -1,42 +1,65 @@
-import psutil
-# you can convert that object to a dictionary
-before = dict(psutil.virtual_memory()._asdict())['percent']
-print("Start Memory Usage:")
-print(before)
+#!/usr/bin/env python
 
-with open ("myins.sql", "r") as fin:
-    with open ("myinsR.sql", "w") as fout:
-        data="S"
-        while(data!=''):
-            data=fin.readline()
+import time
+import filecmp
 
-            data=data.replace("`",'')
-            data=data.replace("0)",'FALSE)')
-            data=data.replace("1)",'TRUE)')
-            data=data.replace("'????'",'NULL')
-            fout.write(data)
-
-print("Memory usage after load in lines:")
-after = dict(psutil.virtual_memory()._asdict())['percent']
-print(after)
-print("Diffference:")
-print(after-before)
-print(" \n")
-print("Start Memory Usage:")
-print(before)
+INPUT_PATH = "myins.sql"
 
 
-with open ("myins.sql", "r") as fin:
-    with open ("myinsR.sql", "w") as fout:
-        data= fin.read()
-        data=data.replace("`",'')
-        data=data.replace("0)",'FALSE)')
-        data=data.replace("1)",'TRUE)')
-        data=data.replace("'????'",'NULL')
+def timed(function):
+    '''timing decorator'''
+    def wrapper(*args, **kwargs):           # we don't know about arguments...
+        start = time.time()
+        result = function(*args, **kwargs)  # ...so we just pass what we got
+        runtime = time.time() - start
+        print('%s took %s seconds' % (function.__name__, runtime))
+        return result
+    return wrapper
+
+
+@timed
+def solution_one():
+    '''
+    Load complete file into memory, then perform replacements and write back
+    to file. Needs a lot of memory, but is fast because we only do replacements
+    once
+    '''
+    with open(INPUT_PATH, "r") as fin:
+        data = fin.read()
+
+    data = data.replace("`","")
+    data = data.replace("0)","FALSE)")
+    data = data.replace("1)","TRUE)")
+    data = data.replace("'????'","NULL")
+
+    with open("myinsR_ONE.sql", "w") as fout:
         fout.write(data)
 
-print("Memory usage after full load:")
-after = dict(psutil.virtual_memory()._asdict())['percent']
-print(after)
-print("Difference:")
-print(after-before)
+@timed
+def solution_two():
+    '''
+    Only read one line at time, do replacements and write line to output file.
+    Needs nearly no memory, but takes longer than solution_one because of more
+    replacement operations called
+    '''
+    with open(INPUT_PATH, "r") as fin:
+
+        with open("myinsR_TWO.sql", "w") as fout:
+
+            data = fin.readline()
+            while data:
+                data = data.replace("`","")
+                data = data.replace("0)","FALSE)")
+                data = data.replace("1)","TRUE)")
+                data = data.replace("'????'","NULL")
+                fout.write(data)
+                data = fin.readline()
+
+
+# seperate operations using sleep to create more readable plot hwen using
+# memory_profiler
+solution_one()
+time.sleep(2)
+solution_two()
+time.sleep(2)
+assert filecmp.cmp("myinsR_ONE.sql", "myinsR_TWO.sql"), 'Result files differ!'
